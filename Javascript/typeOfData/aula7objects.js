46c3ff0916110451d35f83cb5477cbde47699bf4
Marcelo <marceloggarciajr@gmail.com>
2022-02-28 20:28:33 -0300

    Javascript

---
+++ Javascript/typeOfData/aula7objects.js
@@ -0,0 +1,15 @@
+console.log({
+     name:"Marcelo",
+     age: "27",
+     andar: function(){
+         console.log('andar')
+     }
+})
+
+//objeto está dentro de chaves {}
+// eles são criados por propriedades/atributos como nome:"Marcelo"
+// nome é o propriedade e marcelo é o atributo
+
+/* funcionalidade é andar e ali tem uma função que é um método,
+ logo andar vai ser um método */
+
