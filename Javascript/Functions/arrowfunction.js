aeaacd3ca63921725b1f6de76b7009e8da55725d
marcelojr <marceloggarciajr@gmail.com>
2022-03-10 23:15:29 -0300

    Aulas dias 10

---
+++ Javascript/Functions/arrowfunction.js
@@ -0,0 +1,9 @@
+const sayMyName = () => {
+    console.log('Marcelo')
+}
+
+sayMyName()
+
+/* Na arrow function substituimos o function por uma seta => '() => {}' 
+*/
+ 
---
+++ Javascript/Functions/calculadora1.js
@@ -0,0 +1,23 @@
+
+
+const sum = function(number1, number2){
+    return number1+number2
+}
+const sub = function(number1, number2){
+    return number1-number2
+    
+}
+const multp = function(number1,number2){
+   return  number1*number2
+}
+const division = function(div1, div2){
+    return div1/div2 
+}
+sum(1,2)
+
+sub(2,1)
+
+multp(3,1)
+
+division(4,1)
+
